3837b22d4b43db09d9e86d2aaa1982ef99f4da06
David Worley <davidworley@gmail.com>
2015-12-05 10:19:31 -0800

    Successful add product test

--- ProductCatalog/tests.py
+++ ProductCatalog/tests.py
@@ -19,6 +19,9 @@ class CatalogUserTestCase(LiveServerTestCase):
             value=59.95
         )
 
+    def find_id(self, id):
+        return self.browser.find_element_by_id(id)
+
     def test_user_can_land(self):
         """
         Test that a user can visit the landing page and see the catalog.
@@ -29,13 +32,13 @@ class CatalogUserTestCase(LiveServerTestCase):
         home_page = self.browser.get(self.live_server_url + '/')
 
         # user should see some basic markup
-        logo = self.browser.find_element_by_id('logo')
+        logo = self.find_id('logo')
         self.assertEqual('Product Catalog', logo.text)
 
-        item_list = self.browser.find_element_by_id('catalog-items')
+        item_list = self.find_id('catalog-items')
         self.assertIsNotNone(item_list)
 
-    def test_user_can_visit_add_product(self):
+    def test_user_can_add_product(self):
         """
         Test that a user can:
             - navigate to the add product page
@@ -45,8 +48,25 @@ class CatalogUserTestCase(LiveServerTestCase):
         """
 
         add_product_url = reverse('addproduct')
-        add_product_page = self.browser.get('%s%s' % (self.live_server_url, add_product_url))
+        self.browser.get('%s%s' % (self.live_server_url, add_product_url))
         self.assertIn('/product/add/', self.browser.current_url)
+
+        add_product_form = self.find_id('product-form')
+        self.assertIsNotNone(add_product_form)
+
+        product_title_text = 'Add Product Test Product'
+        self.find_id('id_name').send_keys(product_title_text)
+        self.find_id('id_description').send_keys('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus enim ex, tincidunt a auctor nec, sodales sit amet massa.')
+        self.find_id('id_width').send_keys('1.25')
+        self.find_id('id_length').send_keys('3.45')
+        self.find_id('id_height').send_keys('5.75')
+        self.find_id('id_weight').send_keys('12.125')
+        self.find_id('id_value').send_keys('49.95')
+
+        self.browser.find_element_by_xpath('//input[@value="Add Product"]').click()
+
+        product_title = self.browser.find_element_by_css_selector('h2')
+        self.assertEqual(product_title_text, product_title.text)
 
     def tearDown(self):
         self.browser.quit()
